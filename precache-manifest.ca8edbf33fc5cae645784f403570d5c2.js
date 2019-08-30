7f029711cdff19b934e641e071477e4d63587a9c
swain <iamswain@hotmail.com>
2019-08-29 18:48:07 -0700

    Updates

---
+++ precache-manifest.ca8edbf33fc5cae645784f403570d5c2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5444b2d5156605c0ccd6f4900117a3",
+    "url": "/parti-blog/index.html"
+  },
+  {
+    "revision": "49d38e11fd02c8ec4c79",
+    "url": "/parti-blog/static/css/2.ec9d628c.chunk.css"
+  },
+  {
+    "revision": "7f4017c0a19fa1edb400",
+    "url": "/parti-blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49d38e11fd02c8ec4c79",
+    "url": "/parti-blog/static/js/2.09c9617d.chunk.js"
+  },
+  {
+    "revision": "7f4017c0a19fa1edb400",
+    "url": "/parti-blog/static/js/main.53e254bb.chunk.js"
+  },
+  {
+    "revision": "04ba2d0749c03b9de33e",
+    "url": "/parti-blog/static/js/runtime~main.da776406.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/parti-blog/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/parti-blog/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/parti-blog/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/parti-blog/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/parti-blog/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/parti-blog/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/parti-blog/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/parti-blog/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/parti-blog/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/parti-blog/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/parti-blog/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/parti-blog/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/parti-blog/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/parti-blog/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/parti-blog/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/parti-blog/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
